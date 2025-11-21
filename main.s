2f1da00b18d750611dccfb42709630af3c073da4
Krishivagrawal2007 <krishivagrawal7@gmail.com>
2025-11-21 11:56:13 +0000

    FINISHED SMASH!!!

--- main.s
+++ main.s
@@ -5,8 +5,7 @@
 ;|*     Property off: Krishiv Agrawal              *|
 ;|*     Student ID: K25000843                      *|
 ;|*     Weighting: 15% of Final Module Grade       *|
-;|==================================================|   
-
+;|==================================================|  
 
 ;|--------------------------------------------------|
 ;|                Define Registers                  |
@@ -22,7 +21,7 @@
 ;---------------------------
 .equ DIGITS, 0x0100    ; store digits of 25000843
 .equ KA,   DIGITS + 8  ; store characters 'k' '.' 'a' = '11' '27' '1'
-.equ KRI,   KA + 3   ; store characters morse code for 'k' 'r' 'i'
+.equ KRI,   KA + 3   ; store characters morse code for 'k' 'r' 'i'/ 'i' 'r' 'k'
 
 ;---------------------------
 ; Code Segment
@@ -46,11 +45,10 @@ main:
     LDI r16, 0xF0
     OUT DDRD, r16
 
-    rcall task1
-    rcall task2
-    rcall task3
-    rcall task4
-    rjmp main
+    ; Start task execution
+    RCALL task4
+
+    rjmp main   ; Loop forever
 
 ;|--------------------------------------------------|
 ;|                   Help Methods                   |
@@ -70,6 +68,7 @@ main:
 ; Final delay:           15,999,999 + (3 * 2) + 1 + (3* 2) + 4 = 16,000,016 cycles = ~1s
 ; Used Ai to compute the values in r21 and r22 and number of NOP statements used for a much better approximate of delay
 precise_delay:
+    ; Add to stack
     PUSH r20
     PUSH r21
     PUSH r22
@@ -100,12 +99,13 @@ inner_loop:
     DEC r20
     BRNE outer_loop
 
+    ; Remove from stack
     POP r22
     POP r21
     POP r20
 
     RET
-    
+   
 ;|--------------------------------------------------|
 ;|                      TASKS                       |
 ;|--------------------------------------------------|
@@ -144,17 +144,17 @@ task1:
 
 loopThroughDigits:
     LD r18, Z+             ; Load value at Z, this increments every loop
-    
+   
     ;Display
     OUT PORTB, r18
     OUT PORTD, r18
-    
+   
     ldi r16, 0x64         ;1sec delay with r16 = 100
-    rcall precise_delay
+    RCALL precise_delay
 
     DEC r17
     BRNE loopThroughDigits  ; Repeats 8 times
-    
+   
     RET
 
 ;|--------------------------
@@ -181,17 +181,17 @@ task2:
 
 loopThroughInitials:
     LD r18, Z+             ; Load value at Z, this increments every loop
-    
+   
     ;Display
     OUT PORTB, r18
     OUT PORTD, r18
-    
+   
     ldi r16, 0x64         ;1sec delay with r16 = 100
-    rcall precise_delay
+    RCALL precise_delay
 
     DEC r17
     BRNE loopThroughInitials  ; Repeats 3 times
-    
+   
     RET
 
 ;|--------------------------
@@ -220,17 +220,17 @@ iterate50Times:
 ;       -Sequence stored in memory
 ;       -Loops through sequence and displays each part of the sequnce until end
 loadIRK:
-    rcall evenSequenceIRK
-    rcall task3DisplaySequence
+    RCALL evenSequenceIRK
+    RCALL task3DisplaySequence
     RJMP afterLoad
 
 loadKRI:
-    rcall oddSequenceKRI
-    rcall task3DisplaySequence
+    RCALL oddSequenceKRI
+    RCALL task3DisplaySequence
 
 afterLoad:
     INC r19
-    CPI r19, 11
+    CPI r19, 51
     BRNE iterate50Times ;Continue iteration
     RET
 
@@ -247,7 +247,7 @@ notDivBy5:
     BREQ display5
     RET
 display5:
-    rcall displayFiveDots
+    RCALL displayFiveDots
     RET
 displayFiveDots:
     LDI r22, 5          ; counter for five dots
@@ -267,22 +267,22 @@ task3DisplaySequence:
 
 loopThroughSequence:
     LD r18, Z+             ; Load value at Z, this increments every loop
-    
+   
     ;Display
-    rcall displayMorseCode
+    RCALL displayMorseCode
 
     DEC r17
     BRNE loopThroughSequence  ; Repeats
 
-    rcall checkDivBy5   ;Add 5 at the end if iteration is a multiple of 5
+    RCALL checkDivBy5   ;Add 5 at the end if iteration is a multiple of 5
 
-    ; Word KRI/IRK/5 has been written and so a gap for 6 units is displayed, 1200ms (1unit already taken when last dot/dash was called) 
+    ; Word KRI/IRK/5 has been written and so a gap for 6 units is displayed, 1200ms (1unit already taken when last dot/dash was called)
     LDI r16, 0x00
     OUT PORTB, r16
     OUT PORTD, r16
     LDI r16, 0x78
-    rcall precise_delay
-    
+    RCALL precise_delay
+   
     RET
 
 oddSequenceKRI:
@@ -351,14 +351,14 @@ dot:
     OUT PORTB, r16
     OUT PORTD, r16
     LDI r16, 0x14
-    rcall precise_delay
+    RCALL precise_delay
 
     ;LED's off for 200 ms
     LDI r16, 0x00
     OUT PORTB, r16
     OUT PORTD, r16
     LDI r16, 0x14
-    rcall precise_delay
+    RCALL precise_delay
     RET
 
 ; Dash turns ON LED for 3 units and OFF for 1 unit
@@ -368,14 +368,14 @@ dash:
     OUT PORTB, r16
     OUT PORTD, r16
     LDI r16, 0x3C
-    rcall precise_delay
-    
+    RCALL precise_delay
+   
     ;LED's off for 200 ms
     LDI r16, 0x00
     OUT PORTB, r16
     OUT PORTD, r16
     LDI r16, 0x14
-    rcall precise_delay
+    RCALL precise_delay
     RET
 
 ; letterGap turns OFF LED for 2 units only, gap between letters of the same word
@@ -385,37 +385,209 @@ letterGap:
     OUT PORTB, r16
     OUT PORTD, r16
     LDI r16, 0x28
-    rcall precise_delay
+    RCALL precise_delay
     RET
 
 ;|--------------------------
 ;|  TASK 4
 ;|--------------------------
-; Start at 10000000 and then using LSR the bit is shifted right giving 01000000
-; Repeat till you reach 00000001 and then start doing LSL to shift bit left resulting in 00000010
-; The process repeats till 10000000 and then it resets and loops forever
+; Implements ping-pong LED movement across the 8-bit output.
+; Movement begins at 10000000 and shifts right until 00000001,
+; then shifts left again. This repeats indefinitely.
+;
+; Special moves:
+;  - Every 5 hits a special move is executed (controlled by r20).
+;         Smash
+;  - WHENEVER any special move completes, SCORE (r23) is incremented
+;    and displayed by blinking the full 8-bit pattern 3 times
+;    (200 ms ON, 200 ms OFF). Score is kept in register r23.
+;
+; Registers used locally in this task:
+;  r17 = current LED bit position
+;  r18 = delay factor (speed)
+;  r19 = speed direction flag (0 = accelerate/increase delay, 1 = decelerate/decrease delay)
+;  r20 = special move countdown / temporary blink counter
+;  r23 = SCORE
+;  r16, r18 are used as scratch for precise_delay (precise_delay expects r16)
 task4:
-    LDI r17, 0x80
+    ; Initialise ping-pong state
+    LDI r17, 0x80      ; r17 = LED position (1000 0000)
+    RCALL resetPingPongLoop
+    LDI r23, 0         ; r23 = SCORE stored in register (start at 1)
 
 pingPongLoopRight:
-    rcall displayPingPong
-    LSR r17
-    CPI r17, 0x01           ; 00000001
-    BREQ pingPongLoopLeft
-    rjmp pingPongLoopRight
-    
-pingPongLoopLeft:
-    rcall displayPingPong
-    LSL r17
-    CPI r17, 0x80           ; 10000000
-    BREQ pingPongLoopRight
-    rjmp pingPongLoopLeft
+    RCALL displayPingPong
+    CPI r17, 0x01           ; reached right end?
+    BREQ updateSpeedRight
+    LSR r17                 ; shift right
+    RJMP pingPongLoopRight
 
+pingPongLoopLeft:
+    RCALL displayPingPong
+    CPI r17, 0x80           ; reached left end?
+    BREQ updateSpeedLeft
+    LSL r17                 ; shift left
+    RJMP pingPongLoopLeft
+
+;------------------------------------------------------
+; Display one frame of the ping-pong LED motion using
+; current position in r17 and speed delay in r18.
+;------------------------------------------------------
 displayPingPong:
     OUT PORTB, r17
     OUT PORTD, r17
-    LDI r16, 0x14
-    rcall precise_delay
+    MOV r16, r18
+    RCALL precise_delay
     RET
 
+;------------------------------------------------------
+; Reached left boundary: decrement special counter and
+; either handle special move or update speed and reverse.
+;------------------------------------------------------
+updateSpeedLeft:
+    DEC r20
+    BREQ handleSpecialMoveLeft
+    RCALL handleSpeed
+    RJMP pingPongLoopRight
 
+;------------------------------------------------------
+; Reached right boundary: decrement special counter and
+; either handle special move or update speed and reverse.
+;------------------------------------------------------
+updateSpeedRight:
+    DEC r20
+    BREQ handleSpecialMoveRight
+    RCALL handleSpeed
+    RJMP pingPongLoopLeft
+
+;------------------------------------------------------
+; Checkontrol helpers
+;------------------------------------------------------
+resetPingPongLoop:
+    LDI r18, 0x0A      ; r18 = speed (delay factor)
+    LDI r19, 0         ; r19 = speed mode (0 = accelerate)
+    LDI r20, 10        ; r20 = special move counter (every 5 hits)
+    RET
+
+resetScore:
+    LDI r23, 0
+    RET
+
+handleSpeed:
+    CPI r18, 1
+    BREQ setDecelerate
+    CPI r18, 0x0A
+    BREQ setAccelerate
+    RCALL handlePingPongSpeed
+    RET
+
+setDecelerate:
+    LDI r19, 0
+    RCALL handlePingPongSpeed
+    RET
+
+setAccelerate:
+    LDI r19, 1
+    RCALL handlePingPongSpeed
+    RET
+
+handlePingPongSpeed:
+    CPI r19, 0
+    BREQ incSpeed
+    DEC r18
+    RET
+
+incSpeed:
+    INC r18
+    RET
+
+;------------------------------------------------------
+; Special-move dispatchers
+; reset r20 to 5 (countdown), branch to smash
+;------------------------------------------------------
+handleSpecialMoveLeft:
+    ; brief pre-burst delay
+    LDI r16, 0x1E
+    RCALL precise_delay
+    RJMP smashLeft
+
+handleSpecialMoveRight:
+    ; brief pre-burst delay
+    LDI r16, 0x1E
+    RCALL precise_delay
+    RJMP smashRight
+
+;------------------------------------------------------
+; Smash Left:
+;  - quick burst (fast movement) shifting right until 0x01
+;  - uses r18 = 1 for very small delay (fast)
+;  - upon completion: increment score (r23), blink score,
+;    toggle r21, and resume ping-pong (moving left->right)
+;------------------------------------------------------
+smashLeft:
+    ; perform fast shifts to the right until we reach 0x01
+    LSR r17
+    LDI r18, 1          ; make movement very fast
+    RCALL displayPingPong
+    CPI r17, 0x01
+    BRNE smashLeft
+
+    ; Special move completed -> score & blink
+    INC r23             ; r23 = r23 + 1
+    CPI r23, 255
+    BREQ resetScore
+
+    RCALL display_score_blink3
+    LDI r17, 0x01
+    RCALL resetPingPongLoop
+    RJMP pingPongLoopLeft
+
+;------------------------------------------------------
+; Smash Right:
+;  - quick burst shifting left until 0x80
+;  - upon completion: increment score, blink score,
+;    toggle r21, and resume ping-pong (moving right->left)
+;------------------------------------------------------
+smashRight:
+    ; perform fast shifts to the left until we reach 0x80
+    LSL r17
+    LDI r18, 1
+    RCALL displayPingPong
+    CPI r17, 0x80
+    BRNE smashRight
+
+    ; Special move completed -> score & blink
+    INC r23
+    CPI r23, 255
+    BREQ resetScore
+    
+    RCALL display_score_blink3
+    LDI r17, 0x80
+    RCALL resetPingPongLoop
+    RJMP pingPongLoopRight
+
+;======================================================
+; display_score_blink3
+; Displays r23 on LEDs and blinks 3 times (200 ms ON / 200 ms OFF)
+; Uses r22 as blink counter and r16/r18 temporarily for delays.
+;======================================================
+display_score_blink3:
+    LDI r22, 3          ; blink counter = 3
+
+blink3_loop:
+    ; ON 200ms
+    OUT PORTB, r23
+    OUT PORTD, r23
+    LDI r16, 0x14
+    RCALL precise_delay
+
+    ; OFF 200ms
+    LDI r24, 0x00
+    OUT PORTB, r24
+    OUT PORTD, r24
+    LDI r16, 0x14
+    RCALL precise_delay
+
+    DEC r22
+    BRNE blink3_loop
+    RET
